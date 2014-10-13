4fdd08ddc5aed28839e4b9975be7a923ef12745c
Korganiof <joonas.korgan@gmail.com>
2014-10-13 21:51:16 +0300

    Here it is

---
+++ app/views/example.blade.php
@@ -0,0 +1,16 @@
+
+/**
+ * Created by PhpStorm.
+ * User: Joonas
+ * Date: 13-Oct-14
+ * Time: 21:13
+ */
+
+ <h1> Dear Lord Otwell </h1>
+ <p> I hereby challenge you to a coding contest! </p>
+
+ <?php echo $squirrel;
+  $ffff = 0;?>
+
+ <p>{{ $ffff  }} </p>
+ <p>{{ date('d/m/y') }}</p>
